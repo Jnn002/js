4c1808709309ea9405cf0bff2afe9142b9fe50e0
John <jyaxp1@miumg.edu.gt>
2024-12-06 21:40:46 -0600

    ADD, .gitignore

--- p6_oop1_shoppingCart/script.js
+++ p6_oop1_shoppingCart/script.js
@@ -87,16 +87,16 @@ const products = [
 
 products.forEach(({ name, id, price, category }) => {
     dessertCards.innerHTML += `
-      <div class="dessert-card">
-        <h2>${name}</h2>
-        <p class="dessert-price">$${price}</p>
-        <p class="product-category">Category: ${category}</p>
-        <button 
-          id="${id}" 
-          class="btn add-to-cart-btn">Add to cart
-        </button>
-      </div>
-    `;
+                            <div class="dessert-card">
+                                <h2>${name}</h2>
+                                <p class="dessert-price">$${price}</p>
+                                <p class="product-category">Category: ${category}</p>
+                                <button 
+                                id="${id}" 
+                                class="btn add-to-cart-btn">Add to cart
+                                </button>
+                            </div>
+                            `;
 });
 
 class ShoppingCart {
@@ -122,13 +122,50 @@ class ShoppingCart {
         currentProductCount > 1
             ? (currentProductCountSpan.textContent = `${currentProductCount}x`)
             : (productsContainer.innerHTML += `
-      <div id="dessert${id}" class="product">
-        <p>
-          <span class="product-count" id="product-count-for-id${id}"></span>${name}
-        </p>
-        <p>${price}</p>
-      </div>
-      `);
+                                        <div id="dessert${id}" class="product">
+                                            <p>
+                                            <span class="product-count" id="product-count-for-id${id}"></span>${name}
+                                            </p>
+                                            <p>${price}</p>
+                                        </div>
+                                        `);
+    }
+
+    getCounts() {
+        return this.items.length;
+    }
+
+    clearCart() {
+        if (!this.items.length) {
+            alert("Your shopping cart is already empty");
+            return;
+        }
+
+        const isCartCleared = confirm("Are you sure you want to clear all items from your shopping cart?");
+
+        if (isCartCleared) {
+            this.items = [];
+            this.total = 0;
+            productsContainer.innerHTML = "";
+            totalNumberOfItems.textContent = 0;
+            cartSubTotal.textContent = 0;
+            cartTaxes.textContent = 0;
+            cartTotal.textContent = 0;
+        }
+    }
+
+    calculateTaxes(amount) {
+        return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
+    }
+
+    calculateTotal() {
+        const subTotal = this.items.reduce((total, item) => total + item.price, 0);
+        const tax = this.calculateTaxes(subTotal);
+        this.total = subTotal + tax;
+        cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
+        cartTaxes.textContent = `$${tax.toFixed(2)}`;
+        cartTotal.textContent = `$${this.total.toFixed(2)}`;
+        return this.total;
     }
 }
 
@@ -136,5 +173,17 @@ const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
 
 [...addToCartBtns].forEach(btn => {
-    btn.addEventListener("click", event => {});
+    btn.addEventListener("click", event => {
+        cart.addItem(Number(event.target.id), products);
+        totalNumberOfItems.textContent = cart.getCounts();
+        cart.calculateTotal();
+    });
 });
+
+cartBtn.addEventListener("click", () => {
+    isCartShowing = !isCartShowing;
+    showHideCartSpan.textContent = isCartShowing ? "Hide" : "Show";
+    cartContainer.style.display = isCartShowing ? "block" : "none";
+});
+
+clearCartBtn.addEventListener("click", cart.clearCart);
