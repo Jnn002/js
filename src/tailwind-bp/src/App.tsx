90e561c3a988f2150710d67e018e4fb29ac54678
John <jyaxp1@miumg.edu.gt>
2024-12-13 23:52:42 -0600

    First Commit, Tailwind bp

---
+++ src/tailwind-bp/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <>
+            <div className="flex flex-wrap justify-center">
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1 className="font-bold text-5xl underline">Vite + React</h1>
+            <div className="card">
+                <button
+                    className="bg-blue-300 m-4 hover:bg-blue-400 p-2 rounded-md text-white font-bold border border-x-blue-200"
+                    onClick={() => setCount(count => count + 1)}
+                >
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        </>
+    );
+}
+
+export default App;
